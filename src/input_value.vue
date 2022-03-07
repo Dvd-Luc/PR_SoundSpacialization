7002f624761456f323b4a3b32a5156bb5c1ca690
Dvd-Luc <79472813+Dvd-Luc@users.noreply.github.com>
2022-03-07 19:13:00 +0100

    grosse refacto du code

---
+++ src/input_value.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    name = "input_value",
+    data() {
+    return {
+      message: ''
+    }
+  }
+}
+</script>
+<template>
+  <p>Message is: {{ message }}</p>
+	<input v-model="message" placeholder="edit me" />
+</template>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -17,18 +17,6 @@
       </q-toolbar>
     </q-header>
 
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
-      <q-list>
-        <q-item-label header> Essential Links </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
-
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -36,8 +24,6 @@
 </template>
 
 <script>
-import EssentialLink from "components/EssentialLink.vue";
-
 const linksList = [
   {
     title: "Docs",
@@ -87,10 +73,6 @@ import { defineComponent, ref } from "vue";
 
 export default defineComponent({
   name: "MainLayout",
-
-  components: {
-    EssentialLink,
-  },
 
   setup() {
     const leftDrawerOpen = ref(false);
